2a7043d81f10f1dda483a6054b2ad023ace081d3
spyatmycode <akejunifemi11@gmail.com>
2023-08-14 15:20:24 +0100

    fix(): server error

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const  bodyParser  =require("body-parser");
+const bodyParser = require("body-parser");
 const cors = require("cors")
 const axios = require("axios")
 require("dotenv").config()
@@ -16,33 +16,37 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.post("/send",(req,res)=>{
-    const {content,number} = req.body
+app.post("/send", async (req, res) => {
+  const { content, number } = req.body;
 
-    const requestBody = {
-      to: number,
-      message: content,
-      sender_name: 'Sendchamp',
-      route: 'dnd'
+  const requestBody = {
+    to: number,
+    message: content,
+    sender_name: 'Sendchamp',
+    route: 'dnd'
+  };
+
+  console.log(requestBody);
+
+  const config = {
+    headers: {
+      Authorization: process.env.SENDCHAMP_AUTH
     }
-  
-    console.log(requestBody);
-    
-    const config = {
-      headers:{
-        Authorization: process.env.SENDCHAMP_AUTH
-    }
+  };
+
+  try {
+    const response = await axios.post('https://api.sendchamp.com/api/v1/sms/send', requestBody, config);
+    console.log(response.data);
+
+    return res.status(200).json({ success: true, msg: "Alert Created successfully", content: content });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ success: false, msg: "An error occurred" });
   }
-  
-    axios.post('https://api.sendchamp.com/api/v1/sms/send', requestBody, config).then(()=>console.log("Thank God"))
-    .catch((err)=> console.log(err))
-    
-  
-    res.json()
-})
+});
 
-const port =  process.env.PORT;
+const port = process.env.PORT;
 
-app.listen(port,()=>{
-  console.log(`Server is listening`);
+app.listen(port, () => {
+  console.log(`Server is listening on ${port}`);
 })
